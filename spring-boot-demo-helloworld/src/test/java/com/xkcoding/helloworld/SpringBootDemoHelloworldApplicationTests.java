2ac1155fa04f74155ba69fca806efe42d82ddced
SeekerGAO <1532584997@qq.com>
2019-11-15 12:00:06 +0800

    使用MockMvc对象模拟HTTP请求

--- spring-boot-demo-helloworld/src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplicationTests.java
+++ spring-boot-demo-helloworld/src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplicationTests.java
@@ -1,16 +1,45 @@
 package com.xkcoding.helloworld;
 
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class SpringBootDemoHelloworldApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void after() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void sayHello() throws Exception {
+    // 使用MockMvc对象模拟HTTP请求
+    String result = mockMvc.perform(
+      get("/hello").param("who", "gao")
+    )
+      .andExpect(status().isOk())
+      .andReturn().getResponse().getContentAsString();
+
+    log.info("返回的结果：{}", result);
+  }
 
 }
